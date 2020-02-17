abde5938948e597900143eceb600585bb86eeacc
ericnord <ericnord88@gmail.com>
2020-02-17 14:40:42 -0600

    Create Chapter 1 Demo.R

---
+++ Chapter 1 Demo.R
@@ -0,0 +1,123 @@
+#############################
+## Chapter  1. Basics 
+### The console, the editor, and basic syntax in R
+
+2 + 2
+4*5
+6^2
+3+5*2
+(3+5)*2
+# 2+2
+
+  
+
+
+a = 5# = 
+
+a
+
+a*2
+a
+a=a*2
+
+
+
+sms=c(0,1,2,0,0,0,1)
+
+sms + 5
+sms * 5
+sms/2
+sort(sms)
+
+
+
+sms[3]
+sms[2:4]
+sms[-3]
+sms[-(2:3)]
+sms[-c(2,3,7)]
+
+sms>0
+sms[sms>0]
+
+sum(sms>0)
+
+which(sms>0)
+sms[which(sms>0)]
+mean(sms[which(sms>0)])
+
+sms
+sms[1]=1
+sms
+
+1:50
+
+# in a typical R environment each line of the above output would being the the index of the first value on the line, so:
+# [1] 1 2 3 ....
+#[29] 29 30 31 ...
+
+x=c("a","b","c","d","e","f","g","h","i")
+y=21:30
+z=c(2,4,6)
+
+x[y]
+
+x[z]
+
+y[z]
+z[y]
+
+x[rev(z)]
+y[x]
+
+y*z
+
+# Function | Description
+# ---------|------------
+# sd()     | standard deviation
+# median() | median
+# max()    | maximum
+# min()    | minimum
+# range()  |maximum and minimum
+# length() | number of elements of a vector
+# cummin() | cumulative min (or max cummax() )
+# diff()   | differences between successive elements of a vector
+
+
+# Loops
+for (i in 1:27) {cat(letters[i])};cat("\n") # cat("\n") just adds a new line
+
+for (i in 1:10) {print(paste(i,"squared =",i^2))}
+
+for (i in sample(x = 1:26,size = 14)) cat(LETTERS[i])
+
+# The same examples w/o using for( )
+paste(letters[1:27],collapse="") # first example, see what happens if paste() is not used
+paste(1:10,"squared =",(1:10)^2)# second example, formatting is different
+sample(LETTERS,size=14) # third example 
+
+x<-c("Most","loops","in","R","can","be","avoided")
+for (i in x){cat(paste(i,"!"))}
+
+###### VI. Exercises. ######
+
+##1)## Enter the following data in R and give it a name (`P1` for example):   
+# `23,45,67,46,57,23,83,59,12,64`  
+# a) What is the maximum value? b) What is the minimum value? c) What is the mean value?
+
+##2)## Oh no! The next to last (9th) value was mistyped - it should be 42.  
+# a) Change the 12 to 42. b) How does this change the mean? c) How many values are greater than 40? d) What is the mean of values over 40?
+# *Hint*: how do you see the 9th element of the vector `P1`?
+  
+##3)## Using the data from problem **2** (after the 9th value was changed) find:  
+# a) the sum of `P1`  
+# b) the mean (using the sum and `length(P1)`)  
+# c) the log(base10) of `P1` - use `log(base=10)`  
+# d) the difference between each element of `P1` and the mean of `P1` (Note: you should not need a loop for this problem).
+
+##4)## If we  have two vectors, `a<-11:20` and `b<-c(2,4,6,8)` predict (_without running the code_) the outcome of the following (Write out your predictions as comments in your HW. After you make your predictions, you can check yourself by running the code). Were you correct? 
+# a) `a*2`  
+# b) `a[b]`  
+# c) `b[a]`  
+# d) `c(a,b)`  
+# e) `a+b` 
